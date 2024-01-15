867d9e22ccc02643fff5031d75fe94e97921c899
Zney4 <134953901+Zney4@users.noreply.github.com>
2024-01-15 19:02:18 +0300

    Update api.py

--- api.py
+++ api.py
@@ -4,8 +4,9 @@ from dotenv import load_dotenv
 from urllib.parse import urlparse
 
 
-def shorten_link(url, api):
-    headers = {"Authorization": "Bearer {}".format(api)}
+def shorten_link(bitlink_token):
+    url = "https://api-ssl.bitly.com/v4/bitlinks"
+    headers = {"Authorization": "Bearer {}".format(bitlink_token)}
 
     params = {"long_url": long_url}
     response = requests.post(url, headers=headers, json=params)
@@ -14,8 +15,8 @@ def shorten_link(url, api):
     return response.json()["id"]
 
 
-def count_clicks(api, bitlink):
-    headers = {"Authorization": "Bearer {}".format(api)}
+def count_clicks(bitlink_token, bitlink):
+    headers = {"Authorization": "Bearer {}".format(bitlink_token)}
     link = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary"
     params = {"units": -1, "unit": "month"}
     response = requests.get(link, params=params, headers=headers)
@@ -34,15 +35,15 @@ if __name__ == "__main__":
     load_dotenv()
 
     long_url = input("Введите ссылку \n>>>")
-    url = "https://api-ssl.bitly.com/v4/bitlinks"
-    api = os.getenv("api")
+
+    bitlink_token = os.getenv("BITLINK_TOKEN")
     try:
-        bitlink = shorten_link(url, api)
+        bitlink = shorten_link(bitlink_token)
     except requests.exceptions.HTTPError as ex:
         print(ex)
 
     print("Битлинк", bitlink)
 
     parsed_path_bitlink = parsed_url(bitlink)
-    count_clicks = count_clicks(api, bitlink)
+    count_clicks = count_clicks(bitlink_token, bitlink)
     print("Колличество кликов:", count_clicks)
