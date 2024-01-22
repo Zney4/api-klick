7c4db3ff98e9a19905b541ef7a8067ef4d6893a1
Zney4 <134953901+Zney4@users.noreply.github.com>
2024-01-22 18:22:14 +0300

    Update api.py

--- api.py
+++ api.py
@@ -6,44 +6,60 @@ from urllib.parse import urlparse
 
 def shorten_link(bitlink_token):
     url = "https://api-ssl.bitly.com/v4/bitlinks"
-    headers = {"Authorization": "Bearer {}".format(bitlink_token)}
+    Authorization = {"Authorization": "Bearer {}".format(bitlink_token)}
 
     params = {"long_url": long_url}
-    response = requests.post(url, headers=headers, json=params)
+    response = requests.post(url, headers=Authorization, json=params)
     response.raise_for_status()
 
     return response.json()["id"]
 
 
 def count_clicks(bitlink_token, bitlink):
-    headers = {"Authorization": "Bearer {}".format(bitlink_token)}
+    Authorization = {"Authorization": "Bearer {}".format(bitlink_token)}
     link = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary"
     params = {"units": -1, "unit": "month"}
-    response = requests.get(link, params=params, headers=headers)
+    response = requests.get(link, params=params, headers=Authorization)
     response.raise_for_status()
 
     return response.json()["total_clicks"]
 
 
+def is_bitlink(long_url, bitlink_token):
+    Authorization = {"Authorization": "Bearer {}".format(bitlink_token)}
+    info_url = f'https://api-ssl.bitly.com/v4/bitlinks/{long_url}'
+
+    response = requests.get(info_url, headers=Authorization)
+
+    return response.status_code
+
+
 def parsed_url(bitlink):
     parsed_bitlink = urlparse(bitlink)
 
-    return parsed_bitlink.netloc + parsed_bitlink.path
+    return f'{parsed_bitlink.netloc}{parsed_bitlink.path}'
 
 
 if __name__ == "__main__":
     load_dotenv()
+    bitlink_token = os.environ["BITLINK_TOKEN"]
 
     long_url = input("Введите ссылку \n>>>")
 
-    bitlink_token = os.getenv("BITLINK_TOKEN")
+    status_code = is_bitlink(long_url, bitlink_token)
+    print('info_url:', status_code)
+
     try:
-        bitlink = shorten_link(bitlink_token)
+        if status_code == 200:
+            count_clicks = count_clicks(bitlink_token, long_url)
+            print("Колличество кликов:", count_clicks)
+        else:
+            bitlink = shorten_link(bitlink_token)
+            count_clicks = count_clicks(bitlink_token, long_url)
+            print("Колличество кликов:", count_clicks)
+
     except requests.exceptions.HTTPError as ex:
         print(ex)
+        print("Битлинк", bitlink)
 
-    print("Битлинк", bitlink)
-
-    parsed_path_bitlink = parsed_url(bitlink)
-    count_clicks = count_clicks(bitlink_token, bitlink)
-    print("Колличество кликов:", count_clicks)
+        parsed_path_bitlink = parsed_url(bitlink)
