7595075a6c611eb8aecb61cb86e2771496931558
Zney4 <134953901+Zney4@users.noreply.github.com>
2024-01-24 19:01:06 +0300

    Update api.py

--- api.py
+++ api.py
@@ -4,37 +4,36 @@ from dotenv import load_dotenv
 from urllib.parse import urlparse
 
 
-def shorten_link(bitlink_token):
+def shorten_link(bitlink_token, long_url):
     url = "https://api-ssl.bitly.com/v4/bitlinks"
-    Authorization = {"Authorization": "Bearer {}".format(bitlink_token)}
-
+    authorization = {"Authorization": "Bearer {}".format(bitlink_token)}
     params = {"long_url": long_url}
-    response = requests.post(url, headers=Authorization, json=params)
+    response = requests.post(url, headers=authorization, json=params)
     response.raise_for_status()
 
     return response.json()["id"]
 
 
-def count_clicks(bitlink_token, bitlink):
-    Authorization = {"Authorization": "Bearer {}".format(bitlink_token)}
+def count_clicks(bitly_token, bitlink):
+    authorization = {"Authorization": "Bearer {}".format(bitly_token)}
     link = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary"
     params = {"units": -1, "unit": "month"}
-    response = requests.get(link, params=params, headers=Authorization)
+    response = requests.get(link, params=params, headers=authorization)
     response.raise_for_status()
 
     return response.json()["total_clicks"]
 
 
-def is_bitlink(long_url, bitlink_token):
-    Authorization = {"Authorization": "Bearer {}".format(bitlink_token)}
+def is_bitlink(long_url, bitly_token):
+    authorization = {"Authorization": "Bearer {}".format(bitly_token)}
     info_url = f'https://api-ssl.bitly.com/v4/bitlinks/{long_url}'
 
-    response = requests.get(info_url, headers=Authorization)
+    response = requests.get(info_url, headers=authorization)
 
     return response.status_code
 
 
-def parsed_url(bitlink):
+def get_parsed_url(bitlink):
     parsed_bitlink = urlparse(bitlink)
 
     return f'{parsed_bitlink.netloc}{parsed_bitlink.path}'
@@ -42,24 +41,24 @@ def parsed_url(bitlink):
 
 if __name__ == "__main__":
     load_dotenv()
-    bitlink_token = os.environ["BITLINK_TOKEN"]
+    bitly_token = os.environ["BITLINK_TOKEN"]
 
     long_url = input("Введите ссылку \n>>>")
 
-    status_code = is_bitlink(long_url, bitlink_token)
+    status_code = is_bitlink(long_url, bitly_token)
     print('info_url:', status_code)
 
     try:
         if status_code == 200:
-            count_clicks = count_clicks(bitlink_token, long_url)
+            count_clicks = count_clicks(bitly_token, long_url)
             print("Колличество кликов:", count_clicks)
         else:
-            bitlink = shorten_link(bitlink_token)
-            count_clicks = count_clicks(bitlink_token, long_url)
+            bitlink = shorten_link(bitly_token, long_url)
+            count_clicks = count_clicks(bitly_token, long_url)
             print("Колличество кликов:", count_clicks)
 
     except requests.exceptions.HTTPError as ex:
         print(ex)
         print("Битлинк", bitlink)
 
-        parsed_path_bitlink = parsed_url(bitlink)
+        parsed_path_bitlink = get_parsed_url(bitlink)
